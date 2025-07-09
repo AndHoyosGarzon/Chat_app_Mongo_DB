598d60f06b8502f0af14f0ebbeac650b6ffbcc48
AndreHG <andreshg9203@gmail.com>
2025-07-09 22:22:31 +1200

    add new styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import Login from "./pages/login/Login";
+/* import Signup from "./pages/signup/Signup";
+import Home from "./pages/home/Home";
+ */
 function App() {
-  return <></>;
+  return (
+    <div className="p-4 h-sreen items-center justify-center">
+      <Login />
+      {/* <Home />
+      <Signup />
+    */}
+    </div>
+  );
 }
 
 export default App;
